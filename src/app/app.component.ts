58ea07988817de138fe921aef57340b4edbb3bbd
ogicesur <oguzhan_cesur_2007@hotmail.com>
2024-12-08 13:14:49 +0100

    feat: add completed project files to GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal, effect, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Product } from './product';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ProductListComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-signals-example';
+
+  allProducts: Product[] = [
+    { id: 1, name: 'ahmet', price: 100 },
+    { id: 2, name: 'mehmet', price: 200 },
+    { id: 3, name: 'mal', price: 300 }
+  ];
+
 }
